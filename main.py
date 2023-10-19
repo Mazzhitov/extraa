3a5b0e6a1c6015636c7b83b6ae17e4e8c601c709
Islam <biko2454@gmail.com>
2023-10-19 12:59:01 +0600

    labka

---
+++ main.py
@@ -0,0 +1,21 @@
+x = ("Anketa")
+first_name = input("first_name")
+last_name = input("last_name")
+date_year = input("date_year")
+national = input("national")
+address = input("address")
+status = input("status")
+mobile_number = input("number")
+
+full_name = first_name + last_name
+age = 2023 - int(date_year)
+
+print(x)
+print("full_name:", first_name + last_name)
+print("age:", age)
+print("national:", national)
+print("address:", address)
+print("status:", status)
+print("mobile_number:", mobile_number)
+
+
